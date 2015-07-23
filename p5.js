8b4597277e03c1347193f47b753c621b25c339eb
Virginia <virginia.hoskins@gmail.com>
2015-07-23 17:27:38 -0600

    HTML changes for program 5, slight js changes

---
+++ p5.js
@@ -0,0 +1,12 @@
+setClickHandler("MakeNames" , newNames);
+
+console.log("Groucho Chico Harpo".split(" "));
+
+function newNames(){
+
+}
+
+var x = 5;
+var f = function(y) {var x = 10; alert(x);}
+f(100)
+alert(x);
