e097041df29664903b9e52e2ebaf31e7f68408b8
Virginia Walter <vjwalt26@Virginias-iMac.home>
2015-07-25 13:55:23 -0600

    changes for program 5

--- p5.js
+++ p5.js
@@ -8,8 +8,11 @@ function newNames(){
 			console.log(NameFirst);
 		var NameLast = ["Potter", "Granger", "Weasley ", "Black ", "Dumbledore ", "Snape ", "Lovegood" , (getInput("LastNames").split(" "))];
 			console.log(NameLast);
-		var NameCreate = NameFirst[Math.floor(Math.random() * NameFirst.length)];
+		var FirstLog = NameFirst[Math.floor(Math.random() * NameFirst.length)];
+			console.log(FirstLog);
+		var LastLog = NameLast[Math.floor(Math.random() * NameLast.length)];
+			console.log(LastLog);
+		var NameCreate = FirstLog + LastLog;
 			console.log(NameCreate);
-		
 }
 
