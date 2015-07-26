4125ec2de72689db97bd1ede020d4722fdd95a9d
Virginia Walter <vjwalt26@Virginias-iMac.home>
2015-07-25 23:41:53 -0600

    changes to spirit animal

--- p5.js
+++ p5.js
@@ -3,16 +3,20 @@ setClickHandler("MakeNames" , newNames);
 
 function newNames(){
 	var NameGet = getInput("NameNum");
-		console.log(NameGet);
-		var NameFirst = ["Harry", " Hermione", " Ron", " George", " Albus", " Sirius", " Luna" , (getInput("FirstNames").split(" "))];
-			console.log(NameFirst);
-		var NameLast = ["Potter", "Granger", "Weasley ", "Black ", "Dumbledore ", "Snape ", "Lovegood" , (getInput("LastNames").split(" "))];
-			console.log(NameLast);
+		//console.log(NameGet);
+		var NameFirst = ["Cowardly", " Presumptuous", " Condescending", " Fat", " Bumbling", " Laughing", " Snobby" , (getInput("FirstNames").split(" "))];
+			//console.log(NameFirst);
+		var NameLast = ["Ant", "Tuna", "Clam ", "Tiger ", "Giraffe ", "Zebra ", "Sparrow" , (getInput("LastNames").split(" "))];
+			//console.log(NameLast);
 		var FirstLog = NameFirst[Math.floor(Math.random() * NameFirst.length)];
-			console.log(FirstLog);
+			//console.log(FirstLog);
 		var LastLog = NameLast[Math.floor(Math.random() * NameLast.length)];
-			console.log(LastLog);
-		var NameCreate = FirstLog + LastLog;
+			//console.log(LastLog);
+		var NameCreate = FirstLog + " " + LastLog;
 			console.log(NameCreate);
+		for (i = NameCreate; i < NameGet; i++){
+			console.log(i);
+		}
+			
 }
 
